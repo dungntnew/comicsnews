7942ac3740f68608d55a0fe0bc5b3cd3e8d5970e
EC2 Default User <ec2-user@ip-172-31-23-166.us-west-2.compute.internal>
2017-09-25 14:01:09 +0900

    remove etc config

---
+++ uwsgi.py
@@ -0,0 +1,6 @@
+from comicnews import app as application
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0',
+            port=8080, debug=True
+            )
---
+++ uwsgi_start.sh
@@ -0,0 +1,1 @@
+uwsgi --http :4000 --wsgi-file uwsgi.py --master --processes 1 --threads 1
